afbba86b21aee87753ff037707bed40855a52285
Daniel Bayley <daniel.bayley@me.com>
2024-04-11 22:55:26 +0100

    Replace Node util/types

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
 
 /* MAIN */
 
+import {types} from 'node:util';
 import isArguments from './is_arguments';
 import isArray from './is_array';
-import isArrayBuffer from './is_array_buffer';
 import isArrayLike from './is_array_like';
 import isArrayLikeObject from './is_array_like_object';
 import isArrowFunction from './is_arrow_function';
-import isAsyncFunction from './is_async_function';
 import isAsyncGeneratorFunction from './is_async_generator_function';
 import isAsyncIterable from './is_async_iterable';
 import isAttribute from './is_attribute';
 import isBigInt from './is_bigint';
-import isBigInt64Array from './is_bigint64array';
-import isBigUint64Array from './is_biguint64array';
 import isBlob from './is_blob';
 import isBoolean from './is_boolean';
 import isBoundFunction from './is_bound_function';
 import isBoxedBigInt from './is_boxed_bigint';
 import isBoxedBoolean from './is_boxed_boolean';
 import isBoxedNumber from './is_boxed_number';
-import isBoxedPrimitive from './is_boxed_primitive';
 import isBoxedString from './is_boxed_string';
 import isBoxedSymbol from './is_boxed_symbol';
 import isBuffer from './is_buffer';
 import isClass from './is_class';
 import isComment from './is_comment';
-import isDataView from './is_data_view';
-import isDate from './is_date';
 import isDefined from './is_defined';
 import isDocument from './is_document';
 import isDocumentFragment from './is_document_fragment';
@@ -39,18 +33,11 @@ import isEven from './is_even';
 import isFalsy from './is_falsy';
 import isFinite from './is_finite';
 import isFloat from './is_float';
-import isFloat32Array from './is_float32array';
-import isFloat64Array from './is_float64array';
 import isFrozen from './is_frozen';
 import isFunction from './is_function';
-import isGeneratorFunction from './is_generator_function';
-import isInt16Array from './is_int16array';
-import isInt32Array from './is_int32array';
-import isInt8Array from './is_int8array';
 import isInteger from './is_integer';
 import isIterable from './is_iterable';
 import isLength from './is_length';
-import isMap from './is_map';
 import isNaN from './is_nan';
 import isNative from './is_native';
 import isNegativeZero from './is_negative_zero';
@@ -64,59 +51,42 @@ import isObjectLike from './is_object_like';
 import isOdd from './is_odd';
 import isPlainObject from './is_plain_object';
 import isPrimitive from './is_primitive';
-import isPromise from './is_promise';
 import isPrototype from './is_prototype';
-import isRegExp from './is_regexp';
 import isSealed from './is_sealed';
 import isSafeInteger from './is_safe_integer';
-import isSet from './is_set';
-import isSharedArrayBuffer from './is_shared_array_buffer';
 import isString from './is_string';
 import isSymbol from './is_symbol';
 import isText from './is_text';
 import isTruthy from './is_truthy';
-import isTypedArray from './is_typed_array';
-import isUint16Array from './is_uint16array';
-import isUint32Array from './is_uint32array';
-import isUint8Array from './is_uint8array';
-import isUint8ClampedArray from './is_uint8clampedarray';
 import isUndefined from './is_undefined';
-import isWeakMap from './is_weak_map';
 import isWeakRef from './is_weak_ref';
 import isWeakReferable from './is_weak_referable';
-import isWeakSet from './is_weak_set';
 import isWindow from './is_window';
 
 /* EXPORT */
 
 export {
+  ...types,
   isArguments,
   isArray,
-  isArrayBuffer,
   isArrayLike,
   isArrayLikeObject,
   isArrowFunction,
-  isAsyncFunction,
   isAsyncGeneratorFunction,
   isAsyncIterable,
   isAttribute,
   isBigInt,
-  isBigInt64Array,
-  isBigUint64Array,
   isBlob,
   isBoolean,
   isBoundFunction,
   isBoxedBigInt,
   isBoxedBoolean,
   isBoxedNumber,
-  isBoxedPrimitive,
   isBoxedString,
   isBoxedSymbol,
   isBuffer,
   isClass,
   isComment,
-  isDataView,
-  isDate,
   isDefined,
   isDocument,
   isDocumentFragment,
@@ -128,18 +98,11 @@ export {
   isFalsy,
   isFinite,
   isFloat,
-  isFloat32Array,
-  isFloat64Array,
   isFrozen,
   isFunction,
-  isGeneratorFunction,
-  isInt16Array,
-  isInt32Array,
-  isInt8Array,
   isInteger,
   isIterable,
   isLength,
-  isMap,
   isNaN,
   isNative,
   isNegativeZero,
@@ -153,26 +116,15 @@ export {
   isOdd,
   isPlainObject,
   isPrimitive,
-  isPromise,
   isPrototype,
-  isRegExp,
   isSealed,
   isSafeInteger,
-  isSet,
-  isSharedArrayBuffer,
   isString,
   isSymbol,
   isText,
   isTruthy,
-  isTypedArray,
-  isUint16Array,
-  isUint32Array,
-  isUint8Array,
-  isUint8ClampedArray,
   isUndefined,
-  isWeakMap,
   isWeakRef,
   isWeakReferable,
-  isWeakSet,
   isWindow
 };
