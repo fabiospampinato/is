0895de76a7d4487235a1f6904727d818668bb996
Fabio Spampinato <spampinabio@gmail.com>
2025-01-25 18:27:39 +0000

    isSafeInteger: hard-coding the min safe integer

--- src/is_safe_integer.ts
+++ src/is_safe_integer.ts
@@ -7,12 +7,13 @@ import isNumber from './is_number';
 /* HELPERS */
 
 const MAX_SAFE_INTEGER = 9007199254740991;
+const MIN_SAFE_INTEGER = -9007199254740991;
 
 /* MAIN */
 
 const isSafeInteger = ( value: unknown ): boolean => {
 
-  return isNumber ( value ) && isInteger ( value ) && ( value >= 0 ? value <= MAX_SAFE_INTEGER : value >= -MAX_SAFE_INTEGER );
+  return isNumber ( value ) && isInteger ( value ) && ( value >= 0 ? value <= MAX_SAFE_INTEGER : value >= MIN_SAFE_INTEGER );
 
 };
 
